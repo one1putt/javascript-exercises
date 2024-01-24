b7de6fe4ab58a8e120878451e9b3a809c4c0b23b
one1putt <klericksonmn@gmail.com>
2024-01-24 08:55:42 -0600

    JS Exercises
    completed 12 javascript Exercises;

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let age
+    const year = new Date().getFullYear()
+    const ages = people.map( (person) => {
+        if (!(person.yearOfDeath)) {
+            age = year - person.yearOfBirth
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        return age;
+    })
+    
+    let max = Math.max(...ages)
 
+    return people[ages.indexOf(max)]
 };
 
 // Do not edit below this line
