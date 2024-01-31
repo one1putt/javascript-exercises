9d7a0fcf0379657bd9191cb9c9c14e183190401b
one1putt <klericksonmn@gmail.com>
2024-01-30 22:04:08 -0600

    solutions added.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+  let total = 0
+  for (let arg of args[0]) {
+    total += Number(arg)
+  }
+  let type = typeof total
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let total = Number(args[0][0])
+  for (let i = 1; i < args[0].length; i++) {
+    total *= Number(args[0][i])
+  }
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let total = a
+  for (let i = 1; i < b; i++) {
+    total *= a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (!number) return 1
+  let total = 1
+  for (let i = 2; i <= number; i++) {
+    total *= i
+  }
+  return total
 };
 
 // Do not edit below this line
